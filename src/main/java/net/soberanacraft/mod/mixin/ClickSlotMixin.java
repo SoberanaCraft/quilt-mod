89fe002309a16a8ca201d827fa5a2802d9575eaa
roridev <roridev@protonmail.com>
2023-03-18 14:26:11 -0300

    Blocks read-only users from dropping items

--- src/main/java/net/soberanacraft/mod/mixin/ClickSlotMixin.java
+++ src/main/java/net/soberanacraft/mod/mixin/ClickSlotMixin.java
@@ -2,6 +2,7 @@ package net.soberanacraft.mod.mixin;
 
 import net.minecraft.network.packet.c2s.play.ButtonClickC2SPacket;
 import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.GameMode;
@@ -22,6 +23,16 @@ public class ClickSlotMixin {
 	public void onClickSlot(ClickSlotC2SPacket packet, CallbackInfo ci) {
 		if(this.player.interactionManager.getGameMode() == GameMode.ADVENTURE) {
 			ci.cancel();
+		}
+	}
+
+	@Inject(method = "onPlayerAction", at = @At("HEAD"), cancellable = true)
+	public void onPlayerAction(PlayerActionC2SPacket packet, CallbackInfo ci) {
+		if (packet.getAction() == PlayerActionC2SPacket.Action.DROP_ITEM
+				|| packet.getAction() == PlayerActionC2SPacket.Action.DROP_ALL_ITEMS) {
+			if(this.player.interactionManager.getGameMode() == GameMode.ADVENTURE) {
+				ci.cancel();
+			}
 		}
 	}
 }
